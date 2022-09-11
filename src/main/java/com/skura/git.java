b5dcd1e77a13a3675fd8dcecf9a1ef952061e2d9
skura <sj13110697393@icloud.com>
2022-09-12 04:34:55 +0800

    third commit

--- src/main/java/com/skura/git.java
+++ src/main/java/com/skura/git.java
@@ -7,5 +7,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hi hi hi");
+        System.out.println("嗨嗨嗨");
     }
 }
